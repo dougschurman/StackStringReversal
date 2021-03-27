7b816b6db52554333abea878e997de96ad922d0e
dougschurman <schurmandoug@hotmail.com>
2021-03-27 01:14:38 -0400

    First commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using StackADT;
+
+namespace ReversingString
+{
+    class ReverseString
+    {
+        static void Main(string[] args)
+        {
+            string mystr = "bruh";
+            Console.WriteLine(ReverseStr(mystr));
+
+        }
+
+        static public string ReverseStr(string input)
+        {
+            Stack s = new Stack(input.Length);
+            char[] characters = input.ToCharArray();
+            for(int i=0; i<characters.Length; i++)
+            {
+                s.Push(characters[i]);
+            }
+            s.PrintStack();
+            for (int j = 0; j < s.GetSize(); j++)
+            {
+                characters[j] = s.Pop();
+            }
+            string output = new string(characters);
+            return output;
+        }
+    }
+}
---
+++ Stack.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace StackADT
+{
+    class Stack
+    {
+        private char[] data_;
+        private int top;
+        private int max;
+        public Stack(int size)
+        {
+            data_ = new char[size];
+            top = -1;
+            max = size;
+        }
+
+        public Stack(Stack stack)
+        {
+            data_ = stack.data_;
+            top = stack.top;
+            max = stack.max;
+        }
+
+        ~Stack()
+        {
+
+        }
+
+        public void Push(char item)
+        {
+            if(top == max - 1)
+            {
+                Console.WriteLine("Overflow");
+                return;
+            }
+            else
+            {
+                data_[++top] = item;
+            }
+        }
+
+        public char Pop()
+        {
+            if(top == -1)
+            {
+                Console.WriteLine("Empty");
+                return '0';
+            }
+            else
+            {
+                return data_[top--];
+            }
+        }
+
+        public int GetSize()
+        {
+            return max;
+        }
+
+        public void PrintStack()
+        {
+            for(int i=top; i>-1; i--)
+            {
+                Console.WriteLine(data_[i]);
+            }
+        }
+    }
+}
+
